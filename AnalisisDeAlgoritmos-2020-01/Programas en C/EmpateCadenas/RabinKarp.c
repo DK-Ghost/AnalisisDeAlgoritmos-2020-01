a4d6654f91d12e40e3fdbe21d7eb1b9ff900c1b1
DK-Ghost <53837804+DK-Ghost@users.noreply.github.com>
2019-11-14 13:22:29 -0600

    Add files via upload

---
+++ AnalisisDeAlgoritmos-2020-01/Programas en C/EmpateCadenas/RabinKarp.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <string.h>
+#define d 117
+
+void buscar (char patron[], char texto[], int primo);
+
+int
+main ()
+{
+  char texto[500];
+  char patron[50];
+
+  printf ("Ingresar la cadena: ");
+  scanf ("%s", texto);
+
+  printf ("Ingresar el patron: ");
+  scanf ("%s", patron);
+
+  int primo = 11;
+  buscar (patron, texto, primo);
+
+
+  return 0;
+}
+
+void
+buscar (char patron[], char texto[], int primo)
+{
+  int M = strlen (patron);
+  int N = strlen (texto);
+
+  int i, j;
+
+  int p = 0;			// hash de patron
+  int t = 0;			// hash de texto 
+  int h = 1;
+
+  for (i = 0; i < M - 1; i++)
+    h = (h * d) % primo;
+
+  for (i = 0; i < M; i++)
+    {				//Calcular el hash de patron y la primera ventana
+      p = (d * p + patron[i]) % primo;
+      t = (d * t + texto[i]) % primo;
+
+    }
+
+  for (i = 0; i <= N - M; i++)
+    {				// recorremos el texto uno a uno
+
+      if (p == t)
+	{			//Si los hash del la ventana y el patron son 
+	  //iguales, verificamos
+
+	  for (j = 0; j < M; j++)
+	    {
+	      if (texto[i + j] != patron[j])
+		break;
+	    }
+
+	  if (j == M)
+
+	    printf ("Concidencia en %d \n", i);
+	}
+
+      if (i < N - M)
+	{			//Calculamos el hash de la ventana
+	  t = (d * (t - texto[i] * h) + texto[i + M]) % primo;
+
+	  if (t < 0)
+	    t = (t + primo);
+	}
+    }
+}
